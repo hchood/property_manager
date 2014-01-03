b0db428630caadba138e70987ee413757c6cd377
Helen Hood <helen.c.hood@gmail.com>
2014-01-03 16:37:04 -0500

    Add factory for Building

---
+++ spec/support/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :building do
+    street_address '100 Main Street'
+    city 'Boston'
+    state 'MA'
+    zip_code '02118'
+    description 'Mixed commercial/residential space'
+  end
+end
