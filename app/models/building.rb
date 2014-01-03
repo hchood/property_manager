063fee20c3f70bd7c3f3148ff852b4150c8b968f
Helen Hood <helen.c.hood@gmail.com>
2014-01-03 16:58:03 -0500

    Add Building model.  Passes all unit tests.

---
+++ app/models/building.rb
@@ -0,0 +1,8 @@
+class Building < ActiveRecord::Base
+  STATES =  %w(AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
+
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates :state, presence: :true, inclusion: { in: STATES }
+  validates :zip_code, presence: :true, format: { with: /\A\d{5}\z/}
+end
--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Building do
   STATES =  %w(AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
 
-  it { validates_presence_of :street_address }
-  it { validates_presence_of :city }
-  it { validates_presence_of :state }
-  it { validates_presence_of :zip_code }
+  it { should validate_presence_of :street_address }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :state }
+  it { should validate_presence_of :zip_code }
 
   it { should have_valid(:state).when('MA', 'CA', 'NY', 'DC') }
   it { should_not have_valid(:state).when('Ontario', 'XY') }
